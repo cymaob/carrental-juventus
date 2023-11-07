d341ba5329f0f8fdf0c05d8280d3cdf17c7f4d0a
cymaob <128226579+cymaob@users.noreply.github.com>
2023-11-07 21:13:46 +0100

    added header with angular material example

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RentComponent } from './rent/rent.component';
-import { HeaderComponent } from './header/header.component';
 
 const routes: Routes = [
   { path: '', component: RentComponent }, // This is the default route
-  { path: 'header', component: HeaderComponent},
 ];
 
 @NgModule({
