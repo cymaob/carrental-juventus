213bfa017f5937e6a86d8ba3ebfb26df7993447c
cymaob <128226579+cymaob@users.noreply.github.com>
2023-11-07 19:11:31 +0100

    modified routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RentComponent } from './rent/rent.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: RentComponent }, // This is the default route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
