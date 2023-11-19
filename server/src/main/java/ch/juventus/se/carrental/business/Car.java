bb3a0966b96a1e1f2326deab76ad84c3c52ec19b
cymaob <128226579+cymaob@users.noreply.github.com>
2023-11-19 16:07:16 +0100

    work done on business layer

--- server/src/main/java/ch/juventus/se/carrental/business/Car.java
+++ server/src/main/java/ch/juventus/se/carrental/business/Car.java
@@ -1,8 +1,15 @@
 package ch.juventus.se.carrental.business;
 
-public class Car {
+public class Car extends AbstractVehicle{
     protected int doors;
     protected int seats;
     protected String transmission;
     protected String chassis;
+
+    public Car(int doors, int seats, String transmission, String chassis) {
+        this.doors = doors;
+        this.seats = seats;
+        this.transmission = transmission;
+        this.chassis = chassis;
+    }
 }
