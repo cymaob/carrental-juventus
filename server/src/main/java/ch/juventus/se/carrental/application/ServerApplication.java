7c312df2bd5cdbeca343b8b92aeb92f69f8ea560
cymaob <128226579+cymaob@users.noreply.github.com>
2023-12-25 13:41:17 +0100

    added logger

---
+++ server/src/main/java/ch/juventus/se/carrental/application/ServerApplication.java
@@ -0,0 +1,19 @@
+package ch.juventus.se.carrental.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SpringBootApplication
+public class ServerApplication {
+    private static final Logger logger = LoggerFactory.getLogger(ServerApplication.class);
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+        logger.info("Application started");
+
+    }
+
+}
